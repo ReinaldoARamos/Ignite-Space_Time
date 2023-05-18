6149b71d7d19812c586f231e79560f1486f99e88
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-18 11:34:24 -0300

    baixando fonts usando next/fonts/google

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 ">
-      <h1 className="text-zinc-100 text-4xl font-bold">Sua Capsula do tempo</h1>
+      <h1 className="text-4xl font-bold text-zinc-100">Sua Capsula do tempo</h1>
     </div>
   )
 }
