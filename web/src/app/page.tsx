4711fad4ed9149b30e4ed34a3bb26701afb0f600
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-18 11:40:57 -0300

    terminando de configurar o as fonts no next dentro do tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 ">
       <h1 className="text-4xl font-bold text-zinc-100">Sua Capsula do tempo</h1>
+      <h1 className="font-alt text-4xl font-bold text-zinc-100">
+        Sua Capsula do tempo
+      </h1>
     </div>
   )
 }
