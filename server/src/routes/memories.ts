f5e3129296f1be3819ce6bd1b9a7c884fe912a70
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-19 10:32:20 -0300

    criando uma memoria dentro do prisma studio e testando suas requisisões

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -3,8 +3,27 @@ import { prisma } from "../lib/prisma";
 
 
 export async function memoriesRoutes(app: FastifyInstance) {
-    app.get('/users', async () => {
-        const users =  await prisma.user.findMany()
-        
-        return users})
+    app.get('/memories', async () => {
+       const memories = await prisma.memory.findMany({
+        orderBy: {
+            created_at: 'asc'
+        }
+       })
+       
+       return memories
+    })
+
+    app.post('/memories', async () => {
+        const memories = await prisma.memory
+     })
+
+     app.put('/memories/:id', async () => {
+        const memories = await prisma.memory
+     })
+
+     app.delete('/memories/:id', async () => {
+        const memories = await prisma.memory
+     })
+
 }
+
