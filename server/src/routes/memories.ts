d7495b995966f69d67280b28718ae51f1ac14d87
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-19 10:36:59 -0300

    dando um map dentro da memory e retornando so as informações que eu preciso para deixar a req mais leve, além de que usei o excerpt com o comando substring para deixar a memoria em um máximo de 115 caractéres, concatenando com ... no final

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -10,7 +10,13 @@ export async function memoriesRoutes(app: FastifyInstance) {
         }
        })
        
-       return memories
+       return memories.map((memory) => {
+         return {
+            id: memory.id,
+            coverUrl: memory.coverUrl,
+            excerpt: memory.contents.substring(0, 115).concat('...')
+         }
+       })
     })
 
     app.post('/memories', async () => {
