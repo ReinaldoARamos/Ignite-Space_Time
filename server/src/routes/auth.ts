6844270e9f6ef66bb1cc0c4449e11853ed5f6177
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-19 21:11:11 -0300

    criando uma validação para que caso haja usuário novo ele faça um create

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,36 +1,65 @@
 import axios from "axios";
 import fastify, { FastifyInstance } from "fastify";
-import z from 'zod'
+import z from "zod";
+import { prisma } from "../lib/prisma";
 export async function AuthRoutes(app: FastifyInstance) {
-    
-    app.post('/register', async (request) => {
-            const bodySchema  = z.object({
-                code: z.string()
-            })
+  app.post("/register", async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    });
 
-            const {code} = bodySchema.parse(request.body)
+    const { code } = bodySchema.parse(request.body);
 
-            const acessCodeToken = await axios.post(
-                'https://github.com/login/oauth/access_token',
-                null,
-                {
-                     params: {
-                        client_id : process.env.GITHUB_CLIENT_ID,
-                        client_secret : process.env.GITHUB_CLIENT_SECRET,
-                        code,
+    const acessCodeToken = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
 
+    const { access_token } = acessCodeToken.data;
 
-                     },
-                     headers: {
-                        Accept: 'application/json'
-                     }
-                }
-            )
+    const userResponse = await axios.get("http://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
 
-            const {access_token} = acessCodeToken.data
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      avatar_url: z.string(),
+    });
+    const userInfo = userSchema.parse(userResponse.data);
 
-            return {
-                access_token
+    let user = await prisma.user.findUnique({
+      where: {
+        gitHubid: userInfo.id,
+      },
+    });
+
+    if(!user){
+        user = await prisma.user.create({
+            data: {
+                gitHubid: userInfo.id,
+                avatarUrl: userInfo.avatar_url,
+                login: userInfo.login,
+                name: userInfo.name,
+                
             }
-    }) 
+        })
+    }
+    return {
+      user,
+    };
+  });
 }
