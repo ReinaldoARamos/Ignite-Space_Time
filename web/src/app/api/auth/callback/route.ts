e2780a89b99dbc54a5f87a996e685a37f31d944b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-23 16:29:29 -0300

    criando a comunicação de nosso front com o backend

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+  console.log(code)
+}
