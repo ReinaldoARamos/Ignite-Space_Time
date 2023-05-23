cb70dd2e8a096bb16f3332d27cfbfd2373e99a93
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-23 16:32:34 -0300

    criando a comunicação de nosso front com o backend

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,7 +1,14 @@
+import { api } from '@/lib/api'
 import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
-  console.log(code)
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  console.log(token)
 }
