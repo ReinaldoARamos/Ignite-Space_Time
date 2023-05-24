4391cef19488248b119ad2c378a02f4a135cf483
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-24 14:26:16 -0300

    configurando layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,13 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJunjamjuree,
 } from 'next/font/google'
+import { Hero } from '@/components/hero'
+import { Profile } from '@/components/profile'
+import { SignIn } from '@/components/signIn'
+
+import { cookies } from 'next/headers'
+import { Copyright } from '@/components/copyright'
+import { EmptyMemories } from '@/components/emptyMemories'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baijamjuree = BaiJunjamjuree({
@@ -17,12 +24,29 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable}  ${baijamjuree.variable}  bg-gray-900 font-sans text-gray-100`}
       >
         {children}
+        <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
+          {/* left**/}
+          <div className=" relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16 ">
+            {/* blur**/}
+            <div className=" absolute right-0  top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripe" />
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+          <EmptyMemories />
+        </main>
       </body>
     </html>
   )
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,28 +1,5 @@
-import { Copyright } from '@/components/copyright'
 import { EmptyMemories } from '@/components/emptyMemories'
-import { Hero } from '@/components/hero'
-import { Profile } from '@/components/profile'
-import { SignIn } from '@/components/signIn'
-import { cookies } from 'next/headers'
 
 export default function Home() {
-  const isAuthenticated = cookies().has('token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
-      {/* left**/}
-      <div className=" relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16 ">
-        {/* blur**/}
-        <div className=" absolute right-0  top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  rounded-full bg-purple-700 opacity-50 blur-full" />
-
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripe" />
-        {isAuthenticated ? <Profile /> : <SignIn />}
-
-        <Hero />
-
-        <Copyright />
-      </div>
-      <EmptyMemories />
-    </main>
-  )
+  return <EmptyMemories />
 }
