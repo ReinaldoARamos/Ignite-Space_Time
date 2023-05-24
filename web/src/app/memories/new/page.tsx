55f6207c2611e8ead0e70f015d3b3c4883ba696e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-24 16:15:22 -0300

    estilizando o anexo, text area e criando um middleware para barrar caso o cara tente acessar rotas que precisa de autenticação

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -31,12 +31,18 @@ export default function NewMemory() {
               id="isPublic"
               name="isPublic"
               value={'true'}
-              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+              className="h-4 w-4 rounded  border-gray-400 bg-gray-700 text-purple-500"
             />
             tornar memória pública
           </label>
         </div>
-        <input type="file" id="media" className="invisible" />
+        <input type="file" id="media" className="invisible h-0 w-0" />
+        <textarea
+          name="content"
+          spellCheck={false}
+          className="w-full flex-1 resize-none rounded border-0 bg-transparent  p-0  text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0 "
+          placeholder="Fique livre para adicionar fotos, vídeos  e relatos sobre experiencias que você quer lembrar para sempre. "
+        />
       </form>
     </div>
   )
