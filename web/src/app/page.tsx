4161f1ca8847070d6303234a6d4ed305526a3627
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-26 16:43:34 -0300

    resgatando as informações das memorias para colocar na Home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,27 @@
 import { EmptyMemories } from '@/components/emptyMemories'
+import { api } from '@/lib/api'
+import { cookies } from 'next/headers'
 
-export default function Home() {
-  return <EmptyMemories />
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+  const token = cookies().get('token')?.value
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memories = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">{JSON.stringify(memories)}</div>
+  )
 }
